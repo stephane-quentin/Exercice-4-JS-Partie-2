35f54c4e78dfd14eeb0d4f2eaadcd9b939d1b128
Stiouf <stephane.quentin@live.fr>
2018-10-08 15:53:46 +0200

    ex4

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function raz(){
+  document.getElementsByName('lastname')[0].value=''; //getElementsByName récupère la valeur par rapport au nom et le 0 correspond au permier élément
+  document.getElementsByName('firstname')[0].value='';
+  document.getElementsByName('city')[0].value='';
+}
